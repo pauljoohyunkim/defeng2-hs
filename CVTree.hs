71d70e5e5265b4bda4c52b698bd48ec4e1168617
pauljoohyunkim <pauljoohyunkim@gmail.com>
2026-08-31 12:41:53 +0900

    visitTree depth 0

--- CVTree.hs
+++ CVTree.hs
@@ -10,10 +10,15 @@ createTree CVConsonantFormer depth = Node CVConsonantFormer depth [createTree CV
 createTree CVConsonantLatter depth = Node CVConsonantLatter depth [createTree CVConsonantFormer (depth-1)]
 createTree CVVowel depth           = Node CVVowel depth [createTree CVConsonantFormer (depth-1), createTree CVConsonantLatter (depth-1)]
 
--- Tree c_former, v, c_latter, prefix => List of words.
+-- Tree c_former, v, c_latter, prefix_rev => List of words.
 visitTree :: CVTree -> [String] -> [String] -> [String] -> String -> [String]
-visitTree (Node t depth subtrees) c_former v c_latter prefix
-    | t == CVConsonantFormer = 
-    | t == CVConsonantLatter = 
-    | t == CVVowel = 
+visitTree (Node t 0 _) c_former v c_latter prefix
+    | t == CVConsonantFormer = map (prefix++) c_former
+    | t == CVVowel           = map (prefix++) v
+    | t == CVConsonantLatter = map (prefix++) c_latter
+--visitTree (Node t depth subtrees) c_former v c_latter prefix
+--    | t == CVConsonantFormer = concat prefix_rev_list
+--                                where prefix_rev_list = map (\tree -> visitTree tree c_former v c_latter prefix_rev) subtrees
+--    | t == CVVowel = 
+--    | t == CVConsonantLatter = 
 
